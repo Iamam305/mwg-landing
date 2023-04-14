ea519570de4d0d0d65c4800368a65c5d99b54f47
ajitesh <ajitesh314mishra@gmail.com>
2023-04-14 13:41:48 +0530

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,28 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+     
+  <link href="/assets/vendor/aos/aos.css" rel="stylesheet"/>
+  <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"/>
+  <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"/>
+  <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet"/>
+  <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet"/>
+  <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet"/>
+
+  <script defer src="/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script defer src="/assets/vendor/aos/aos.js"></script>
+  <script defer src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script  defer src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script defer src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script defer src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script defer src="/assets/vendor/php-email-form/validate.js"></script>
+  <link defer href="/assets/css/style.css" rel="stylesheet"/>
+
+  <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
